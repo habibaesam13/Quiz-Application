699bd1f21fe469298fdbf3923e287466d35dca71
Habeba Essam <111433697+habibaesam13@users.noreply.github.com>
2023-08-07 14:40:27 +0300

    Add files via upload

---
+++ QuizApp/questions.js
@@ -0,0 +1,53 @@
+//define array of questions
+let questions = [
+    {
+        qnum: 1,
+        question: "What does PHP stand for?",
+        options: [
+            "PHP: Hypertext Preprocessor",
+            "Private Home Page",
+            "Personal Hypertext Processor"
+        ],
+        answer: "PHP: Hypertext Preprocessor"
+    },
+    {
+        qnum: 2,
+        question: "How do you get information from a form that is submitted using the 'get' method?",
+        options: [
+            "Request.QueryString",
+            "$_GET",
+            "Request.Form"
+        ],
+        answer: "$_GET"
+    },
+    {
+        qnum: 3,
+        question: "Which MySQL statement is used to select all data from a table?",
+        options: [
+            "SELECT *",
+            "SELECT",
+            "GET"
+        ],
+        answer: "SELECT *"
+    },
+    {
+        qnum: 4,
+        question: "Which MySQL statement is used to insert new data into a database?",
+        options: [
+            "INSERT INTO",
+            "INSERT NEW",
+            "ADD RECORD"
+        ],
+        answer: "INSERT INTO"
+    },
+    {
+        qnum: 5,
+        question: "What does CSS stand for?",
+        options: [
+            "Cascading Style Sheet",
+            "Common Style Sheet",
+            "Colorful Style Sheet"
+        ],
+        answer: "Cascading Style Sheet"
+    }
+];
